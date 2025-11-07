c3f5f8dd83ea395a05703b19e8eb1e8e4eaf7847
AnaNaybeth0627 <naybeth.medinap@uanl.edu.mx>
2025-11-06 21:43:59 -0600

    Tarea 1
    Código modular de la tarea 1

---
+++ src/acquisition/lectura_y_registro_de_cambios.psm1
@@ -0,0 +1,51 @@
+function LecturaRegistro { 
+    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "$env:USERPROFILE"
+    $watcher.EnableRaisingEvents = $true
+    $watcher.IncludeSubdirectories = $true
+
+    $action =
+    {
+        $logObj = [PSCustomObject]@{
+            timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+            run_id    = [guid]::NewGuid().ToString()
+            module    = "FileWatcher"
+            level     = "INFO"
+            event     = $event.SourceEventArgs.ChangeType
+            details   = $event.SourceEventArgs.FullPath
+        }
+
+        # Mostrar en consola
+        $logObj | Format-Table -AutoSize
+
+
+        $linea = "{0,-20} {1,-10} {2}" -f $logObj.timestamp, $logObj.event, $logObj.details
+        Add-Content -Path "$env:USERPROFILE\reporte_de_cambios.txt" -Value $linea
+
+        $json = $logObj | ConvertTo-Json -Compress
+        Add-Content -Path "$env:USERPROFILE\reporte_de_cambios.json" -Value $json
+    }
+
+    #Registrar eventos
+    $createdEvent = Register-ObjectEvent $watcher 'Created' -Action $action
+    $changedEvent = Register-ObjectEvent $watcher 'Changed' -Action $action
+    $deletedEvent = Register-ObjectEvent $watcher 'Deleted' -Action $action
+    $renamedEvent = Register-ObjectEvent $watcher 'Renamed' -Action $action
+
+    Write-Host "Watcher iniciado. Presiona enter para detenerlo..."
+
+    #Esperar a que se presione el enter
+    [void][System.Console]::ReadLine()
+
+    #liberar watch y desregistar los eventos
+    Unregister-Event -SubscriptionId $createdEvent.Id
+    Unregister-Event -SubscriptionId $changedEvent.Id
+    Unregister-Event -SubscriptionId $deletedEvent.Id
+    Unregister-Event -SubscriptionId $renamedEvent.Id
+    $watcher.Dispose()
+
+    Write-Host "Watcher detenido."
+    Write-Host "Reporte TXT generado."
+    Write-Host "Log JSON generado."
+
+}
